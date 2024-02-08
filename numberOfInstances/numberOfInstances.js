b04abcd30db5086e952044ee6e6f5dc007168d2f
singhalsomesh <159440054+singhalsomesh@users.noreply.github.com>
2024-02-08 22:38:51 +0530

    Number of numberOfInstances

--- numberOfInstances/numberOfInstances.js
+++ numberOfInstances/numberOfInstances.js
@@ -1,14 +1,16 @@
 let count = 0;
 
-function getName(){
+function getName(name){
     count++;
-    return "somesh kumar gupta";
+    return name;
 }
 
-getName()
-getName()
-getName()
-getName()
+let name1 = getName('somesh')
+let name2 = getName('tushar')
+let name3 = getName('ajay')
+let name4 = getName('amrit')
+
+console.log(name1,name2,name3,name4)
 
 console.log(count);
 
